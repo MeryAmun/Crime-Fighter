19ba3db3528d6ac36322abe597739f2506a4281d
Chebesi Awah <chebesiawah@gmail.com>
2020-06-17 21:33:24 +0100

    contact.ejs in progress

--- app.js
+++ app.js
@@ -6,16 +6,23 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html')
+  res.render('index')
 });
 app.get('/contact', function(req, res){
-  res.sendFile('/contact.html')
-});
-app.get('/profile/:name', function(req, res){
   var emergency = [
     {unit: ' Police', location: 'Old Town', contact: 118},
   {unit: ' Hospital', location: 'Molyko ', contact: 117},
   {unit: ' Fire Brigard', location: 'Mulang ', contact: 119},
+  {unit: ' Social service', location: 'Fiango ', contact: 113}
+]
+  res.render('contact',{emergency: emergency});
+  });
+
+app.get('/profile/:name', function(req, res){
+  var socials = [
+    {unit: ' Police', location: 'Old Town', contact: 118},
+  {unit: ' Hospital', location: 'Molyko ', contact: 117},
+  {unit: ' Psychologist',  contact: 129},
   {unit: ' Social service', location: 'Fiango ', contact: 113}
 ]
   res.render('profile', {person: req.params.name, emergency: emergency});
