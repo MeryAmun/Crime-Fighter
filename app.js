6b4e60487dec647794ab71f932b7c036ca82d251
Chebesi Awah <chebesiawah@gmail.com>
2020-06-25 22:35:49 +0100

    more styling and adjustments

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //Remember to create new git repo Crime Fighter
 
 var express = require('express');
-var app = express()
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
@@ -10,23 +13,29 @@ app.get('/', function(req, res){
   res.render('index')
 });
 app.get('/contact', function(req, res){
+
   var emergency = [
-    {unit: ' Police', location: 'Old Town', contact: 118},
-  {unit: ' Hospital', location: 'Molyko ', contact: 117},
+  {unit: ' Police', location: 'Old Town', contact: 118},
+ {unit: ' Hospital', location: 'Molyko ', contact: 117},
   {unit: ' Fire Brigard', location: 'Mulang ', contact: 119},
-  {unit: ' Social service', location: 'Fiango ', contact: 113}
+ {unit: ' Social service', location: 'Fiango ', contact: 113}
 ]
-  res.render('contact',{emergency: emergency});
+  res.render('contact', {qs: req.query, emergency: emergency});
+  });
+
+  app.post('/contact', urlencodedParser, function(req, res){
+
+    res.render('contact-success', {data: req.body});
   });
 
 app.get('/profile/:name', function(req, res){
   var socials = [
-    {unit: ' Police', location: 'Old Town', contact: 118},
-  {unit: ' Hospital', location: 'Molyko ', contact: 117},
-  {unit: ' Psychologist',  contact: 129},
-  {unit: ' Social service', location: 'Fiango ', contact: 113}
+    {unit: ' Counsellor', location: 'Old Town', contact: 118},
+  //{unit: ' Hospital', location: 'Molyko ', contact: 117},
+//  {unit: ' Psychologist',  contact: 129},
+//  {unit: ' Social service', location: 'Fiango ', contact: 113}
 ]
-  res.render('profile', {person: req.params.name, emergency: emergency});
+  res.render('profile', {person: req.params.name, socials: socials});
 });
 
 
